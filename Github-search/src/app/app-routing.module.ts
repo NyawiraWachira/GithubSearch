74b2f2c0b16ac0e36dd18689e290ce7d208b5dc8
ABBY <abbyW@moringa.com>
2022-04-18 19:01:28 +0300

    routing components.

--- Github-search/src/app/app-routing.module.ts
+++ Github-search/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { SearchPageComponent } from './components/search-page/search-page.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component : HomepageComponent
+  },
+
+  {
+    path:'search',
+    component : SearchPageComponent
+  },
+  {
+    path: 'user/:id',
+    component:UserProfileComponent
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
