a8211263957f442a6671e545909f802dd5c96a14
ABBY <abbyW@moringa.com>
2022-04-18 18:27:01 +0300

    service code to get user information from gh api using http client.

--- Github-search/src/app/services/profile.service.ts
+++ Github-search/src/app/services/profile.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  url: string = 'https://api.github.com/users/';
+
+  constructor(private http : HttpClient) { }
+  getUser (username:string) {
+    
+    return this.http.get(this.url+username);
+  }
 }
