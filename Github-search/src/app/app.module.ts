fcfdbde5c72b138bd5eb417ba37a8b860aefa1fa
ABBY <abbyW@moringa.com>
2022-04-18 18:09:21 +0300

    created a search-page componenet.

--- Github-search/src/app/app.module.ts
+++ Github-search/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { SearchPageComponent } from './components/search-page/search-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomepageComponent
+    HomepageComponent,
+    SearchPageComponent
   ],
   imports: [
     BrowserModule,
