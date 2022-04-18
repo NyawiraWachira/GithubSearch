f991221ec3b71c5d3d23c0698ae9db10df10ab78
ABBY <abbyW@moringa.com>
2022-04-18 19:39:54 +0300

    created functions to retrieve github user profile information.

--- Github-search/src/app/components/user-profile/user-profile.component.ts
+++ Github-search/src/app/components/user-profile/user-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute,Router} from '@angular/router';
+import { ProfileService } from 'src/app/services/profile.service';
+
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+  userDetail: any;
+  repos: any;
+
+
+  constructor(private active : ActivatedRoute,private profileService: ProfileService,
+    private route : Router) { }
 
   ngOnInit(): void {
+    this.active.params.subscribe(params=>{
+      this.username=params['id'];
+      console.log("params=",this.username);
+    })
+    this.profileService.getUser(this.username).subscribe({
+      complete:()=>{console.log("successfully done!");
+    },
+      error:()=> {
+        alert("you have entered a wrong username!");
+        this.route.navigate(['search']);
+      },
+      next :(data:any=[])=> {
+        this.userDetail=data;
+        console.log(this.userDetail);
+      }
+
+    })
+    this.profileService.getRepos(this.username).subscribe(repos=>{
+      console.log(repos);
+      this.repos= repos;
+    })
   }
+  
 
 }
